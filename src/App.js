fd989dac2841dd7f0e30cd45c12a99ae05f18e6a
Matthew Foyle <mfoyle93@gmail.com>
2023-10-23 20:10:22 +0200

    feat: v19

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import { DynamicContextProvider, DynamicWidget } from "@dynamic-labs/sdk-react";
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { EthersExtension } from "@dynamic-labs/ethers-v5";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { AlgorandWalletConnectors } from "@dynamic-labs/algorand";
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
+import { FlowWalletConnectors } from "@dynamic-labs/flow";
+import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
+import { CosmosWalletConnectors } from "@dynamic-labs/cosmos";
+import { MagicWalletConnectors } from "@dynamic-labs/magic";
+import { BloctoEvmWalletConnectors } from "@dynamic-labs/blocto";
 
 const App = () => {
   return (
     <DynamicContextProvider
       settings={{
-        environmentId: "8b5038f9-171c-480b-9f3c-294d868aeb74",
+        environmentId: "4e598b41-f388-489b-a0b3-d24064b1d1ed",
+        walletConnectorExtensions: [EthersExtension],
+        walletConnectors: [
+          EthereumWalletConnectors,
+          AlgorandWalletConnectors,
+          SolanaWalletConnectors,
+          FlowWalletConnectors,
+          StarknetWalletConnectors,
+          CosmosWalletConnectors,
+          MagicWalletConnectors,
+          BloctoEvmWalletConnectors,
+        ],
       }}
     >
       <DynamicWagmiConnector>
