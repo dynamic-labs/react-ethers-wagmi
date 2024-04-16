f5b7ce10aa248e3156c9e765630e8c2a1f06b154
Matthew Foyle <mfoyle93@gmail.com>
2024-04-16 12:12:38 +0200

    chore: packages

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import {
 } from "@dynamic-labs/sdk-react-core";
 import { EthersExtension } from "@dynamic-labs/ethers-v5";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
-
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { createConfig, WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { http } from "viem";
+import { mainnet } from "viem/chains";
+
+const config = createConfig({
+  chains: [mainnet],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
@@ -16,9 +30,13 @@ const App = () => {
         walletConnectors: [EthereumWalletConnectors],
       }}
     >
-      <DynamicWagmiConnector>
-        <DynamicWidget />
-      </DynamicWagmiConnector>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>
+            <DynamicWidget />
+          </DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
     </DynamicContextProvider>
   );
 };
